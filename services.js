8bb8c6018f91bec7d54a525f9c522610e842e41a
Stupar92 <stupar.etf@gmail.com>
2016-02-06 18:45:00 +0100

    Added service for getting weather
    Moved from controller to custom service

--- services.js
+++ services.js
@@ -1,4 +1,19 @@
 //SERVICES
 weatherApp.service('cityService', function() {
     this.city = "New York, NY";
-})
+});
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+	
+	this.getWeather = function(city, days) {
+		var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily",
+                                 {callback: "JSON_CALLBACK"},
+                                 {get: {method: "JSONP"}});
+    
+		return weatherAPI.get({
+        	q: city,
+        	cnt: days,
+        	APPID: "4b4741e77fdeefe25b350f9ae2c9dd29"
+    });
+	}
+}]);
